a20aef4f3c7ce9e0d6882e8fd5c2cec955ca6e09
Advait <40948853+adv010@users.noreply.github.com>
2024-02-08 21:20:25 +0100

    add exp_name to tb_log

--- tools/train.py
+++ tools/train.py
@@ -148,7 +148,7 @@ def main():
         os.system('cp %s %s' % (args.cfg_file, output_dir))
 
     tb_log = SummaryWriter(log_dir=str(output_dir / 'tensorboard')) if cfg.LOCAL_RANK == 0 else None
-
+    tb_log.add_text(args.extra_tag,"",0)
     # -----------------------create dataloader & network & optimizer---------------------------
     train_set, train_loader, train_sampler = build_dataloader(
         dataset_cfg=cfg.DATA_CONFIG,
