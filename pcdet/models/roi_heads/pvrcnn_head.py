Bugfix normalization.

pcdet/models/roi_heads/pvrcnn_head.py
@@ -186,9 +186,8 @@ class PVRCNNHead(RoIHeadTemplate):
         pooled_features = self.pool_features(batch_dict)
         batch_size_rcnn = pooled_features.shape[0]
         shared_features = self.shared_fc_layer(pooled_features.view(batch_size_rcnn, -1, 1))
-        if self.model_cfg.DINO_LOSS.NORM_LAST_LAYER:
-            shared_features = F.normalize(shared_features, p=2, dim=-1)
-        proj_feats = self.projector(shared_features.squeeze())
+        shared_norm_features = F.normalize(shared_features.squeeze(), p=2, dim=-1)
+        proj_feats = self.projector(shared_norm_features)
         rcnn_cls = self.cls_layers(shared_features).transpose(1, 2).contiguous().squeeze(dim=1)  # (B, 1 or 2)
         rcnn_reg = self.reg_layers(shared_features).transpose(1, 2).contiguous().squeeze(dim=1)  # (B, C)
 